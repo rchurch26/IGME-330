0d18ed8b732b0fd526b870ec93239cb40e56fbd8
Rahmel Church Jr <rahmelchurch@gmail.com>
2023-09-14 16:42:30 -0400

    90% Done with HW1

--- HWs/church-r-HW1/src/main.js
+++ HWs/church-r-HW1/src/main.js
@@ -1,27 +1,23 @@
 import {getRandomWord} from "./utils.js";
 
-const init = () =>
+generateBabbles();
+
+document.querySelector("#my-button").onclick = () => 
 {
-    const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
-	
-	const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
-	
-    const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
-
     generateBabbles();
-
-    document.querySelector("#my-button").onclick = generateBabbles;
-    document.querySelector("#higher-babbles").onclick = generateBabbles;
-
-    function generateBabbles(num=1)
+}
+document.querySelector("#higher-babbles").onclick = () => 
+{
+    generateBabbles(5);
+}
+function generateBabbles(num=1)
+{
+    document.querySelector("#output").innerHTML = "";
+    // const babble = `${getRandomWord(words1)} ${getRandomWord(words2)} ${getRandomWord(words3)}`;
+    // document.querySelector("#output").innerHTML = babble;
+    for(let i = 0; i < num; i++)
     {
-        // const babble = `${getRandomWord(words1)} ${getRandomWord(words2)} ${getRandomWord(words3)}`;
-        // document.querySelector("#output").innerHTML = babble;
-        for(let i = 0; i < num; i++)
-        {
-            const babble = `${getRandomWord(words1)} ${getRandomWord(words2)} ${getRandomWord(words3)}`;
-            document.querySelector("#output").innerHTML += `\n${babble}`;
-        }
+        const babble = `${getRandomWord(words1)} ${getRandomWord(words2)} ${getRandomWord(words3)}`;
+        document.querySelector("#output").innerHTML += `${babble}<br>`;
     }
 }
-init();
--- HWs/church-r-HW1/src/utils.js
+++ HWs/church-r-HW1/src/utils.js
@@ -1,6 +1,27 @@
-function getRandomWord(array)
+const getRandomWord = (array) => array[Math.floor(Math.random() * array.length)];
+
+const loadBabble = () =>
 {
-    return array[Math.floor(Math.random() * array.length)];
+    const url = "data/babble-data.json";
+    const xhr = new XMLHttpRequest();
+    xhr.onload = (e) =>
+    {
+        console.log(`In onload - HTTP Status Code = ${e.target.status}`);
+        let json;
+        try
+        {
+            json = JSON.parse(e.target.responseText);
+        }
+        catch
+        {
+            document.querySelector("#output").innerHTML = "JSON.parse() failed!";
+            return;
+        }
+
+        const words1 = json.words1;
+        const words2 = json.words2;
+        const words3 = json.words3;
+    }
 }
 
 export {getRandomWord};
