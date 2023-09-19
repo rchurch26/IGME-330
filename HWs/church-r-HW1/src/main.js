9892988011c4a5bc6d52677fe345196ee309d803
Rahmel Church Jr <rahmelchurch@gmail.com>
2023-09-19 15:54:45 -0400

    Added object restructuring to HW1

--- HWs/church-r-HW1/src/main.js
+++ HWs/church-r-HW1/src/main.js
@@ -4,9 +4,10 @@ let words1, words2, words3;
 
 const babbleLoaded = json => 
 {
-    words1 = json.words1;
-    words2 = json.words2;
-    words3 = json.words3;
+    // words1 = json.words1;
+    // words2 = json.words2;
+    // words3 = json.words3;
+    ({words1, words2, words3} = json);
     generateBabbles(1);
     document.querySelector("#my-button").onclick = () => generateBabbles(1);
     document.querySelector("#higher-babbles").onclick = () => generateBabbles(5);
