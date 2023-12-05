57eab4163e828da66dbdf4f3f1e6b23b4ceffb83
rchurch26 <rahmelchurch@gmail.com>
2023-12-04 19:53:30 -0500

    Completed HW4 Starter

--- PEs/church-r-hw4-starter/hw-4-starter/coder-a-hw4/src/main.js
+++ PEs/church-r-hw4-starter/hw-4-starter/coder-a-hw4/src/main.js
@@ -6,9 +6,20 @@ import * as ajax from "./ajax.js";
 const lnglatNYS = [-75.71615970715911, 43.025810763917775];
 const lnglatUSA = [-98.5696, 39.8282];
 let geojson;
+let favoriteIds = ["p20","p79","p180","p43"];
 
 
 // II. Functions
+const refreshFavorites = () =>
+{
+	const favoritesContainer = document.querySelector("#favorites-list");
+	favoritesContainer.innerHTML = "";
+	for(const id of favoriteIds)
+	{
+		favoritesContainer.appendChild(createFavoriteElement(id));
+	}
+};
+
 const setupUI = () => {
 	// NYS Zoom 5.2
 	document.querySelector("#btn-1").onclick = () =>
@@ -34,18 +45,46 @@ const setupUI = () => {
 		map.flyTo(lnglatUSA);
 	}
 
+	refreshFavorites();
 }
 
 const getFeatureById = (id) =>
 {
-	if (geojson.features.find(id)) return features.id;
+	//return geojson.features.find(() => id);
+	for(let i = 0; i < geojson.features.length; i++)
+	{
+		if(geojson.features[i].id == id) return geojson.features[i];
+	}
 }
 
 const showFeatureDetails = (id) => {
 	console.log(`showFeatureDetails - id=${id}`);
 	const feature = getFeatureById(id);
 	document.querySelector("#details-1").innerHTML = `Info for ${feature.properties.title}`;
+	document.querySelector("#details-2").innerHTML = `Address: ${feature.properties.address}<br>
+	Website: <a href = "${feature.properties.url}">${feature.properties.url}</a><br>
+	Phone Number: <a href = "tel:${feature.properties.phone}">${feature.properties.phone}</a>`;
+	document.querySelector("#details-3").innerHTML = `${feature.properties.description}`;
 };
+
+const createFavoriteElement = (id) =>
+{
+	const feature = getFeatureById(id);
+	const a = document.createElement("a");
+	a.className = "panel-block";
+	a.id = feature.id;
+	a.onclick = () =>
+	{
+		showFeatureDetails(a.id);
+		map.setZoomLevel(6);
+		map.flyTo(feature.geometry.coordinates);
+	}
+	a.innerHTML = `<span class="panel-icon">
+	<i class="fas fa-map-pin"></i>
+	</span>
+	${feature.properties.title}`;
+	return a;
+}
 
 const init = () => {
 	map.initMap(lnglatNYS);
